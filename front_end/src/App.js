be0679f06e73a2f9930377af0b91693dabeda48a
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-01-25 18:44:00 +0200

    routes set

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import CultureSelection from './CultureSelection/CultureSelection';
 import LandingPage from './LandingPage/LandingPage'
 import LoginPage from './LoginPage/LoginPage';
 import RecommendedBooks from './RecommendedBooks/RecommendedBooks';
+import ChooseExperience from './ChooseExperience/ChooseExperience.jsx'
+import SearchPage from './search/SearchPage.jsx'
 
 function App() {
   return (
     <div className="App">
-      <CultureSelection />
-      {/* <ChooseExperience /> */}
-      {/* <RecommendedBooks /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="LoginPage" element={<LoginPage />} />
+          <Route path="CultureSelection" element={<CultureSelection />} />
+          <Route path="ChooseExperience" element={<ChooseExperience />} />
+          <Route path="SearchPage" element={<SearchPage />} />
+          <Route path="RecommendedBooks" element={<RecommendedBooks />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
