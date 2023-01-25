9ff8c3502f1a5b047a0c6320867670782490b038
Iliya Zimin <112098758+zimin-iliya@users.noreply.github.com>
2023-01-25 16:38:53 +0200

    context start

--- front_end/src/Contexts/appContext.js
+++ front_end/src/Contexts/appContext.js
@@ -1,19 +1,90 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 
 const AppContext = React.createContext();
+const [signup, setSignup] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    });
+    const [search, setsearch] = useState({
+    search: "",
+    });
+    const [culture, setculture] = useState({
+    const [suggest, setsuggest] = useState([]);
+const [login, setlogin] = useState({
+    email: "",
+    password: "",
+  })
 
 export function useApp() {
     return useContext(AppContext);
 }
 
+const getAllBooks = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.get("http://localhost:8080/");
+      console.log(res.data);
+    } catch (err) {
+      console.log("All Books", err);
+    }
+  };
 
+  const hanleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/signup", signup);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const hanleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.get("http://localhost:8080/search", search);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const hanleSuggest = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/suggest",culture,suggest);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const hanleLogIn = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/login", login);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
 export function AppContextProvider({ children }) {
 
 
 
     const value = {
-   
+        hanleSignUp,
+        hanleLogIn,
+        getAllBooks,
+        hanleSearch,
+        hanleSuggest,
+        setsearch,
+        setlogin,
+        setSignup,
+        setsuggest,
+        setculture,
     }
 
     return (
