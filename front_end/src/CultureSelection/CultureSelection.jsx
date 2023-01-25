da4556d8057119cc74a1ad87bcfc58386c571562
yaelnac <ys.nachumow@gmail.com>
2023-01-25 12:49:18 +0200

    m2

--- front_end/src/CultureSelection/CultureSelection.jsx
+++ front_end/src/CultureSelection/CultureSelection.jsx
@@ -4,7 +4,10 @@ import "./CultureSelection.css"
 const CultureSelection = () => {
   return (
     <div className="culture-selection-container">
-      <div className="go-back-section"></div>
+      <div className="go-back-container">
+        <span></span>
+        <p className="go-back">Go back</p>
+      </div>
       <h2 className="culture-selection-title">Select the culture that interests you</h2>
     </div>
   )
