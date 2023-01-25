3ae3be623fa862318986762ce8797cea604c4d96
Amir Ashlag <ashlag.amir@gmail.com>
2023-01-25 13:28:22 +0200

    update

--- back_end/db_models.js
+++ back_end/db_models.js
@@ -2,5 +2,23 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = "https://aahsweephgfviqmbtqgm.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhaHN3ZWVwaGdmdmlxbWJ0cWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2MzU1NDUsImV4cCI6MTk5MDIxMTU0NX0.LY4gQYAowg3kWTrBuViNpX9UZBy72Fr4dYRlmbkBP3U";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhaHN3ZWVwaGdmdmlxbWJ0cWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2MzU1NDUsImV4cCI6MTk5MDIxMTU0NX0.LY4gQYAowg3kWTrBuViNpX9UZBy72Fr4dYRlmbkBP3U";
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+// async function getallbooks() {
+//   const a = await supabase.from("books").select("*").limit(10);
+//   console.log(a);
+// }
+
+async function checkEmail(req, res, next){
+  const email = req.body.email
+  const result = await supabase.from("users").select('*').eq("email", email)
+  if (result) {
+    next();
+  } else {
+    console.log(result);
+    res.send("email already exists");
+  }
+}
+
+module.exports = { checkEmail };
--- back_end/server.js
+++ back_end/server.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const PORT = 8080;
+const app = express();
+const signup = require('./routs/signup')
+const cors = require("cors");
+
+app.use(cors())
+app.use(express.json());
+
+app.use("/signup", signup);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
